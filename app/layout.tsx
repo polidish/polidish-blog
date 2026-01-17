f0707d185a57bd3470c8a568aaa1c4332b1df45b
polidish <jeni@polidish.com>
2026-01-16 20:37:09 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,20 @@
+import "./globals.css";
 
+export const metadata = {
+title: "Polidish",
+description: "Freedom is deliberate.",
+};
+
+export default function RootLayout({
+children,
+}: {
+children: React.ReactNode;
+}) {
+return (
+<html lang="en">
+<body className="bg-[#E5A55A] text-black antialiased">
+{children}
+</body>
+</html>
+);
+}
